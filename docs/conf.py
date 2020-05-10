350de2114d6d5bd39a5f532a75bfaeb6ff4014a1
mak4515 <49048165+mak4515@users.noreply.github.com>
2020-05-09 20:35:21 -0400

    rtfd

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Michelle A. Kennedy'
 author = 'Michelle A. Kennedy'
 
 # The full version, including alpha/beta/rc tags
-release = 'Beta'
+release = '0.0.1'
 master_doc  = 'index'
 
 # # -- Mock imports ------------------------------------------------------------
